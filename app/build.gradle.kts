5eefab7f0ccf6f41f8d909e9ebd039302ede4e1c
jntorres2014 <32020858+jntorres2014@users.noreply.github.com>
2026-09-13 11:53:54 -0300

    release: bump internal test version to 1.0.3 (6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.jonytorres.huka"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
